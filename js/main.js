abc8c1fd2198330c866b17e52e03943c6cb209bf
Y-jamal05 <302005591@student.rocmondriaan.nl>
2023-11-14 11:39:25 +0100

    begonnen met odprachten 1,2,3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log("main loaded")
+console.log("main loaded");
+
+const person = {
+    firstName: "Yousef",
+    lastName: "Jamal",
+    age: 18,
+    course: "Software Developer",
+    Score: "",
+};
+
+const facebookPost = {
+    userName: "",
+    title: "",
+    story: "",
+    date: "",
+    views: "",
+};
+
+const firstRecept = {
+    title: "Wraps met gegrilde groenten",
+    description: "Heerlijke groente wraps met hummus en belegd met gegrilde groenten en krokant geroosterde kikkererwten, een lekker vegetarisch gerecht.",
+    healthy: "yes",
+};
+
+const secondRecept = {
+    title: "Pasta met kikkererwten en paprikasaus",
+    description: "Pasta met kikkererwten en paprikasaus, een super lekker én super eenvoudig recept dat je in no-time maakt. Je hoeft alleen de saus te maken, tomaatjes te snijden en pasta te koken en je hebt een heerlijke maaltijd.",
+    healthy: "yes",
+};
+
+const thirdRecept = {
+    title: "",
+    description: "",
+    healthy: "",
+};
