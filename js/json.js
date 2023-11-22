f5dc912e2e6f0647f86d8cfb5a84f3c68d6b82bd
Y-jamal05 <302005591@student.rocmondriaan.nl>
2023-11-23 00:21:49 +0100

    tijdelijke fix opdrachten
    opdracht 1 en 4 rare error volgende keer fixen.

--- js/json.js
+++ js/json.js
@@ -1,67 +1,69 @@
 console.log("nothing here");
- 
-// opdracht 1a
-// const person = `
-// {
-//     "firstname": "Judeska",
-//     "email": "judeska@example.com"
-// }
-// `;
- 
-// const objPerson = JSON.parse(person);
-// console.log("Firstname is: ", objPerson.firstname);
-// console.log("email is: ", objPerson.email);
- 
 
-//Opdracht 1b
- 
-// const otherPerson = `{
-//     "id": 1,
-//   "firstname": "John",
-//   "lastname": "Doe",
-//   "email": "johndoe@example.com",
-//   "birthDate": "1973-01-22",
-//   "phone": "(555) 555-1234",
-//   "website": "www.johndoe.com"
-// }
-// `;
- 
-// const objPerson = JSON.parse(otherPerson);
-// console.log("Firstname is: ", objPerson.firstname);
-// console.log("lastname is: ", objPerson.lastname);
-// console.log("email is: ", objPerson.email);
-// console.log("birthdate is: ", objPerson.birthDate);
-// console.log("phone is: ", objPerson.phone);
-// console.log("website is: ", objPerson.website);
+// opdracht 1a
+const person = `
+{
+    "firstname": "Judeska",
+    "email": "judeska@example.com"
+    "zzzzzzzzzzzzzzzzzzzz": "zzzzzzzzzzzzzzzzzzzz"
+}
+`;
+
+const objPerson = JSON.parse(person);
+console.log("Firstname is: ", objPerson.firstname);
+console.log("email is: ", objPerson.email);
+console.log("zzzzzzzzzzzzzzzzzzzz", objPerson.zzzzzzzzzzzzzzzzzzzz);
+
+
+// Opdracht 1b
+const secondPerson = `{
+    "id": 1,
+  "firstname": "John",
+  "lastname": "Doe",
+  "email": "johndoe@example.com",
+  "birthDate": "1973-01-22",
+  "phone": "(555) 555-1234",
+  "website": "www.johndoe.com",
+  "zzzzzzzzzzzzzzzzzzzz": "zzzzzzzzzzzzzzzzzzzz"
+}
+`;
+
+const objSecondPerson = JSON.parse(secondPerson);
+console.log("Firstname is: ", objSecondPerson.firstname);
+console.log("lastname is: ", objSecondPerson.lastname);
+console.log("email is: ", objSecondPerson.email);
+console.log("birthdate is: ", objSecondPerson.birthDate);
+console.log("phone is: ", objSecondPerson.phone);
+console.log("website is: ", objSecondPerson.website);
+console.log("zzzzzzzzzzzzzzzzzzzz", objSecondPerson.zzzzzzzzzzzzzzzzzzzz);
 
 // odpracht 2
+const thirdPerson = `{
+    "id": 1,
+    "slug": "lorem-ipsum",
+    "url": "https://jsonplaceholder.org/posts/lorem-ipsum",
+    "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "content": "Ante taciti nulla sit libero orci sed nam. Sagittis suspendisse gravida ornare iaculis cras nullam varius ac ullamcorper.",
+    "image": "https://dummyimage.com/800x430/FFFFFF/lorem-ipsum.png&text=jsonplaceholder.org",
+    "thumbnail": "https://dummyimage.com/200x200/FFFFFF/lorem-ipsum.png&text=jsonplaceholder.org",
+    "status": "published",
+    "category": "lorem",
+    "publishedAt": "04/02/2023 13:25:21",
+    "updatedAt": "14/03/2023 17:22:20",
+    "userId": 1,
+    "zzzzzzzzzzzzzzzzzzzz": "zzzzzzzzzzzzzzzzzzzz"
+  }
+  `;
 
-// const thirdPerson = `{
-//     "id": 1,
-//     "slug": "lorem-ipsum",
-//     "url": "https://jsonplaceholder.org/posts/lorem-ipsum",
-//     "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-//     "content": "Ante taciti nulla sit libero orci sed nam. Sagittis suspendisse gravida ornare iaculis cras nullam varius ac ullamcorper.",
-//     "image": "https://dummyimage.com/800x430/FFFFFF/lorem-ipsum.png&text=jsonplaceholder.org",
-//     "thumbnail": "https://dummyimage.com/200x200/FFFFFF/lorem-ipsum.png&text=jsonplaceholder.org",
-//     "status": "published",
-//     "category": "lorem",
-//     "publishedAt": "04/02/2023 13:25:21",
-//     "updatedAt": "14/03/2023 17:22:20",
-//     "userId": 1
-//   }
-//   `;
- 
-// const objPerson = JSON.parse(thirdPerson);
-// console.log("id is: ", objPerson.id);
-// console.log("slug is: ", objPerson.slug);
-// console.log("url is: ", objPerson.url);
-// console.log("title is: ", objPerson.title);
-// console.log("content is: ", objPerson.content);
-// console.log("image is: ", objPerson.image);
-
-//odpracht 3
-
+const objThirdPerson = JSON.parse(thirdPerson);
+console.log("id is: ", objThirdPerson.id);
+console.log("slug is: ", objThirdPerson.slug);
+console.log("url is: ", objThirdPerson.url);
+console.log("title is: ", objThirdPerson.title);
+console.log("content is: ", objThirdPerson.content);
+console.log("image is: ", objThirdPerson.image);
+console.log("zzzzzzzzzzzzzzzzzzzz", objThirdPerson.zzzzzzzzzzzzzzzzzzzz);
+// odpracht 3
 const fourthPerson = ` {
     "id": 4,
     "firstname": "Emily",
@@ -93,11 +95,18 @@ const fourthPerson = ` {
       "catchPhrase": "Your success is our priority",
       "bs": "Consulting"
     }
+    "zzzzzzzzzzzzzzzzzzzz": "zzzzzzzzzzzzzzzzzzzz"
   }
-`;  
+`;
 
-const objPerson = JSON.parse(fourthPerson);
-console.log("Firstname is: ", objPerson.firstname);
-console.log("lastname is: ", objPerson.lastname);
-console.log("email is: ", objPerson.email);
-console.log("birthdate is: ", objPerson.birthDate);
+// const objFourthPerson = JSON.parse(fourthPerson);
+// console.log("Firstname is: ", objFourthPerson.firstname);
+// console.log("lastname is: ", objFourthPerson.lastname);
+// console.log("email is: ", objFourthPerson.email);
+// console.log("birthdate is: ", objFourthPerson.birthDate);
+// console.log("login is: ", objFourthPerson.login);
+// console.log("phone is: ", objFourthPerson.phone);
+// console.log("address is: ", objFourthPerson.website);
+// console.log("address is: ", objFourthPerson.address);
+// console.log("company is: ", objFourthPerson.company);
+// console.log("zzzzzzzzzzzzzzzzzzzz", objFourthPerson.zzzzzzzzzzzzzzzzzzzz);
